f76e58c29f4c2ce7d082549b3cdeb213a0e59759
marcin <mar.zybura@gmail.com>
2018-12-30 19:14:14 +0100

    remove comments and redundant variables

--- main.js
+++ main.js
@@ -7,17 +7,13 @@ const gifsContainer = document.querySelector(".gifs-container");
 const giphyForm = document.getElementById("giphy-form");
 
 const callApi = () => {
-  // clearGifs();
   const loader = document.getElementById("loader");
   if (loader.classList.contains("hidden")) {
     loader.classList.remove("hidden");
   }
-  // loader.setAttribute("class", "loader");
-  // gifsContainer.appendChild(loader);
   setTimeout(() => {
     fetch(`${apiUrl}?q=${getInputValue()}&api_key=${apiKey}&limit=${apiLimit}`)
     .then((response) => {
-      // gifsContainer.removeChild(loader);
       return response.json();
     })
     .then((myJson) => {
@@ -36,31 +32,6 @@ const getInputValue = () => {
 const clearGifs = () => {
   gifsContainer.innerHTML = "";
 }
-
-// const addCell = (columnsQuantity) => {
-//   for (let i = 0; i < columnsQuantity; i++) {
-//     const cell = document.createElement("div");
-//     cell.setAttribute("class", "col-sm column__cell");
-//     row.appendChild(cell);
-//     const gifImage = document.createElement("img");
-//     gifImage.setAttribute("class", "gif-image");
-//     cell.appendChild(gifImage);
-//   }
-// }
-
-// const addRow = (rowsNeeded) => {
-//   for (let i = 0; i < rowsNeeded; i++) {
-//     const row = document.createElement("div");
-//     row.setAttribute("class", "row");
-//     gifsContainer.appendChild(row);
-
-//     addCell(columnsQuantity);
-//   }
-// }
-
-// const addRowsAndCells = (rowsNeeded, columnsQuantity) => {
-//   addRow(rowsNeeded);
-// }
 
 const debounce = (func, wait, immediate) => {
   var timeout;
@@ -89,10 +60,6 @@ const keyUpApiCall = debounce(() => {
 }, 200);
 
 const insertGif = (myJson) => {
-  const columnsQuantity = 4;
-  const rowsNeeded = Math.ceil(myJson.data.length / columnsQuantity);
-
-  // addRowsAndCells(rowsNeeded, columnsQuantity);
   for (let i = 0; i < myJson.data.length; i++) {
     const gifImage = document.createElement("img");
     gifImage.setAttribute("class", "gif-image");
